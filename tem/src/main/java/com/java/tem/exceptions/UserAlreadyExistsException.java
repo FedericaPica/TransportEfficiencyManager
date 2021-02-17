6e4e79d60d05284aaa965e9ba503079aa880057d
Foolishota <77018261+Foolishota@users.noreply.github.com>
2021-02-17 12:54:30 +0100

    Check style

--- tem/src/main/java/com/java/tem/exceptions/UserAlreadyExistsException.java
+++ tem/src/main/java/com/java/tem/exceptions/UserAlreadyExistsException.java
@@ -3,7 +3,7 @@ package com.java.tem.exceptions;
 import java.sql.SQLIntegrityConstraintViolationException;
 
 public class UserAlreadyExistsException extends SQLIntegrityConstraintViolationException {
-	public UserAlreadyExistsException(String errorMessage) {
-		super(errorMessage);
-	}
+  public UserAlreadyExistsException(String errorMessage) {
+    super(errorMessage);
+  }
 }
