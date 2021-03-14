8312ba13f460b434bfd7f8a4985f23266c544753
Federica Pica <federicap409@gmail.com>
2021-03-14 05:30:27 +0100

    Update AccountControllerTest.java

--- tem/src/test/java/com/java/tem/controller/AccountControllerTest.java
+++ tem/src/test/java/com/java/tem/controller/AccountControllerTest.java
@@ -57,122 +57,122 @@ class AccountControllerTest {
   public void setup() {
 
     mockMvc = MockMvcBuilders.standaloneSetup(accountController)
-			.setControllerAdvice(new ExceptionHandlingController())
-			.apply(springSecurity())
+        .setControllerAdvice(new ExceptionHandlingController())
+        .apply(springSecurity())
         .build();
   }
 
-	@Test
-	void getIndexTest() throws Exception {
-		Utente utente = new Utente();
-		//mockMvc.perform(post("/process_register")
-		//   .param("rawPassword", "Lel")).andExpect(status().is5xxServerError());
-		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
-	}
+  @Test
+  void getIndexTest() throws Exception {
+    Utente utente = new Utente();
+    //mockMvc.perform(post("/process_register")
+    //   .param("rawPassword", "Lel")).andExpect(status().is5xxServerError());
+    mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
+  }
 
-	/*
-	 * Process Register Tests
-	 */
-	@Test
-	void processRegisterEmptyDenominazione() throws Exception {
-		String url = "/process_register";
-		MvcResult result = mockMvc.perform(post(url).with(csrf())
-				.param("email", "azienda@gmail.com")
-				.param("password", "password")
-				.param("username", "username")
-				.param("denominazione", "")
-				.param("partitaIVA", "08100750010")
-				.param("telefono", "08257654334")
-				.param("fax", "800909396")
-				.param("indirizzo", "via Roma n. 13")
-				.param("cap", "89700")
-				.param("citta", "Verona")).andReturn();
-		String sizeErrorString = "size must be between 2 and 50";
-		Object bindingResObject = result.getModelAndView().getModelMap()
-				.getAttribute("org.springframework.validation.BindingResult.dettaglioUtente");
-		BindingResult bindingResult = (BindingResult) bindingResObject;
-		assertTrue(
-				bindingResult.getFieldError("denominazione").toString().contains(sizeErrorString),
-				"");
+  /*
+   * Process Register Tests
+   */
+  @Test
+  void processRegisterEmptyDenominazione() throws Exception {
+    String url = "/process_register";
+    MvcResult result = mockMvc.perform(post(url).with(csrf())
+        .param("email", "azienda@gmail.com")
+        .param("password", "password")
+        .param("username", "username")
+        .param("denominazione", "")
+        .param("partitaIVA", "08100750010")
+        .param("telefono", "08257654334")
+        .param("fax", "800909396")
+        .param("indirizzo", "via Roma n. 13")
+        .param("cap", "89700")
+        .param("citta", "Verona")).andReturn();
+    String sizeErrorString = "size must be between 2 and 50";
+    Object bindingResObject = result.getModelAndView().getModelMap()
+        .getAttribute("org.springframework.validation.BindingResult.dettaglioUtente");
+    BindingResult bindingResult = (BindingResult) bindingResObject;
+    assertTrue(
+        bindingResult.getFieldError("denominazione").toString().contains(sizeErrorString),
+        "");
 
-	}
+  }
 
-	@Test
-	void processRegisterUserAlreadyExists() throws Exception {
-		String url = "/process_register";
-		when(accountService.checkUserExistanceByEmail(ArgumentMatchers.anyString()))
-				.thenReturn(Boolean.TRUE);
-		mockMvc.perform(post(url).with(csrf())
-				.param("email", "azienda@gmail.com")
-				.param("password", "password")
-				.param("username", "username")
-				.param("denominazione", "Bus s.r.l")
-				.param("partitaIVA", "08100750010")
-				.param("telefono", "08257654334")
-				.param("fax", "800909396")
-				.param("indirizzo", "via Roma n. 13")
-				.param("cap", "89700")
-				.param("citta", "Verona")).andExpect(status().is3xxRedirection())
-				.andExpect(result -> assertTrue(result.getModelAndView().getModelMap()
-								.getAttribute("error").toString().contains("Utente già esistente"),
-						"Utente esistente"));
-	}
+  @Test
+  void processRegisterUserAlreadyExists() throws Exception {
+    String url = "/process_register";
+    when(accountService.checkUserExistanceByEmail(ArgumentMatchers.anyString()))
+        .thenReturn(Boolean.TRUE);
+    mockMvc.perform(post(url).with(csrf())
+        .param("email", "azienda@gmail.com")
+        .param("password", "password")
+        .param("username", "username")
+        .param("denominazione", "Bus s.r.l")
+        .param("partitaIVA", "08100750010")
+        .param("telefono", "08257654334")
+        .param("fax", "800909396")
+        .param("indirizzo", "via Roma n. 13")
+        .param("cap", "89700")
+        .param("citta", "Verona")).andExpect(status().is3xxRedirection())
+        .andExpect(result -> assertTrue(result.getModelAndView().getModelMap()
+                .getAttribute("error").toString().contains("Utente già esistente"),
+            "Utente esistente"));
+  }
 
-	@Test
-	void processRegisterOk() throws Exception {
-		String url = "/process_register";
-		when(accountService.checkUserExistanceByEmail(ArgumentMatchers.anyString()))
-				.thenReturn(Boolean.FALSE);
+  @Test
+  void processRegisterOk() throws Exception {
+    String url = "/process_register";
+    when(accountService.checkUserExistanceByEmail(ArgumentMatchers.anyString()))
+        .thenReturn(Boolean.FALSE);
 
-		Utente utente = new Utente();
-		DettaglioUtente dettaglioUtente = new DettaglioUtente();
-		utente.setDettaglio(dettaglioUtente);
-		utente.setEmail("azienda@gmail.com");
+    Utente utente = new Utente();
+    DettaglioUtente dettaglioUtente = new DettaglioUtente();
+    utente.setDettaglio(dettaglioUtente);
+    utente.setEmail("azienda@gmail.com");
 
-		when(accountService.registerUser(utente, dettaglioUtente)).thenReturn(utente);
+    when(accountService.registerUser(utente, dettaglioUtente)).thenReturn(utente);
 
-		mockMvc.perform(post(url).with(csrf())
-				.param("email", "azienda@gmail.com")
-				.param("password", "password")
-				.param("username", "username")
-				.param("denominazione", "Bus s.r.l")
-				.param("partitaIVA", "08100750010")
-				.param("telefono", "08257654334")
-				.param("fax", "800909396")
-				.param("indirizzo", "via Roma n. 13")
-				.param("cap", "89700")
-				.param("citta", "Verona")).andExpect(status().isOk())
-				.andExpect(view().name("register_success"));
-	}
+    mockMvc.perform(post(url).with(csrf())
+        .param("email", "azienda@gmail.com")
+        .param("password", "password")
+        .param("username", "username")
+        .param("denominazione", "Bus s.r.l")
+        .param("partitaIVA", "08100750010")
+        .param("telefono", "08257654334")
+        .param("fax", "800909396")
+        .param("indirizzo", "via Roma n. 13")
+        .param("cap", "89700")
+        .param("citta", "Verona")).andExpect(status().isOk())
+        .andExpect(view().name("register_success"));
+  }
 
-	/* End of Process register tests */
+  /* End of Process register tests */
 
 
-	@Test
-	@WithMockUser
-	void testUsersPageNoAdmin() throws Exception {
-		when(accountService.isAdmin()).thenReturn(false);
-		String urlString = "/users";
-		mockMvc.perform(get(urlString)).andExpect(status().isForbidden()).andExpect(
-				result -> assertTrue(
-						result.getResolvedException() instanceof NotAuthorizedException));
-	}
+  @Test
+  @WithMockUser
+  void testUsersPageNoAdmin() throws Exception {
+    when(accountService.isAdmin()).thenReturn(false);
+    String urlString = "/users";
+    mockMvc.perform(get(urlString)).andExpect(status().isForbidden()).andExpect(
+        result -> assertTrue(
+            result.getResolvedException() instanceof NotAuthorizedException));
+  }
 
-	@Test
+  @Test
   @WithMockUser
   void testUsersPageAdmin() throws Exception {
-		when(accountService.isAdmin()).thenReturn(true);
-		List<Utente> listUsers = new ArrayList<Utente>();
-		when(accountService.getAllUsers()).thenReturn(listUsers);
-		String urlString = "/users";
-		mockMvc.perform(get(urlString)).andDo(print()).andExpect(status().isOk())
-				.andExpect(view().name("admin"));
-	}
+    when(accountService.isAdmin()).thenReturn(true);
+    List<Utente> listUsers = new ArrayList<Utente>();
+    when(accountService.getAllUsers()).thenReturn(listUsers);
+    String urlString = "/users";
+    mockMvc.perform(get(urlString)).andDo(print()).andExpect(status().isOk())
+        .andExpect(view().name("admin"));
+  }
 
-	@Test
+  @Test
   @WithMockUser
   void testHomeNotLogged() {
 
-	}
+  }
 
 }
